106d2fc954e165b4ab3c74742475d6c9ca943117
Matthew Turk <matthewturk@gmail.com>
2022-05-18 10:40:16 -0500

    Run stuff through the linter

--- src/twitch-embed.d.ts
+++ src/twitch-embed.d.ts
@@ -7,85 +7,84 @@
 //declare module 'twitch-embed';
 
 declare global {
-    interface Window {
-        Player: Player;
-        Embed: Embed;
-        embedded: Embed;
-    }
+  interface Window {
+    Player: Player;
+    Embed: Embed;
+    embedded: Embed;
+  }
 }
 
 export interface TwitchEmbedParameters {
-    allowfullscreen: boolean;
-    channel: string;
-    collection?: string;
-    height: number | string;
-    layout?: string;
-    parent?: string[];
-    theme?: string;
-    video?: string;
-    width: number | string;
+  allowfullscreen: boolean;
+  channel: string;
+  collection?: string;
+  height: number | string;
+  layout?: string;
+  parent?: string[];
+  theme?: string;
+  video?: string;
+  width: number | string;
 }
 
 export interface TwitchPlayerParameters {
-    channel: string;
-    video: string;
-    collection: string;
-    height: number | string;
-    parent: string[];
-    playerdivID: string;
-    width: number | string;
+  channel: string;
+  video: string;
+  collection: string;
+  height: number | string;
+  parent: string[];
+  playerdivID: string;
+  width: number | string;
 
-    playsinline?: boolean;
+  playsinline?: boolean;
 }
 
 declare class Player {
-    constructor(divId: string, playerOptions: TwitchPlayerParameters);
+  constructor(divId: string, playerOptions: TwitchPlayerParameters);
 
-    /* Playback API */
-    pause(): void;
-    play(): void;
-    seek(timestamp: number): void;
-    setChannel(channel: string): void;
-    setCollection(collectionID: string, videoID: string): void;
-    setQuality(quality: string): void;
-    setVideo(videoID: string, timestamp: number): void;
+  /* Playback API */
+  pause(): void;
+  play(): void;
+  seek(timestamp: number): void;
+  setChannel(channel: string): void;
+  setCollection(collectionID: string, videoID: string): void;
+  setQuality(quality: string): void;
+  setVideo(videoID: string, timestamp: number): void;
 
-    /* Volume API */
-    getMuted(): boolean;
-    setMuted(muted: boolean): void;
-    getVolume(): number;
-    setVolume(volumeLevel: number): void;
+  /* Volume API */
+  getMuted(): boolean;
+  setMuted(muted: boolean): void;
+  getVolume(): number;
+  setVolume(volumeLevel: number): void;
 
-    /* Status API */
-    getChannel(): string;
-    getCurrentTime(): number;
-    getDuration(): number;
-    getEnded(): boolean;
-    getQualities(): string[];
-    getQuality(): string;
-    getVideo(): string;
-    isPaused(): boolean;
+  /* Status API */
+  getChannel(): string;
+  getCurrentTime(): number;
+  getDuration(): number;
+  getEnded(): boolean;
+  getQualities(): string[];
+  getQuality(): string;
+  getVideo(): string;
+  isPaused(): boolean;
 
-    addEventListener(event: string, callback: Function): void;
+  addEventListener(event: string, callback: Function): void;
 
-    ENDED: string;
-    PAUSE: string;
-    PLAY: string;
-    PLAYBACK_BLOCKED: string;
-    PLAYING: string;
-    OFFLINE: string;
-    ONLINE: string;
-    READY: string;
-
+  ENDED: string;
+  PAUSE: string;
+  PLAY: string;
+  PLAYBACK_BLOCKED: string;
+  PLAYING: string;
+  OFFLINE: string;
+  ONLINE: string;
+  READY: string;
 }
 
 declare class Embed {
-    constructor(divId: string, options: TwitchEmbedParameters);
+  constructor(divId: string, options: TwitchEmbedParameters);
 
-    addEventListener(eventType: string, callback: Function): void;
+  addEventListener(eventType: string, callback: Function): void;
 
-    getPlayer(): Player;
+  getPlayer(): Player;
 
-    VIDEO_PLAY: string;
-    VIDEO_READY: string;
+  VIDEO_PLAY: string;
+  VIDEO_READY: string;
 }
